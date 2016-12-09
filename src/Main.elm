a40a625459b68e51f4ed8d0737bd40ad5a1d6f68
Jessica Kerr <jessitron@gmail.com>
2016-12-08 18:58:09 -0600

    commit it all right now

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,6 @@
 module Main exposing (main)
 
-import Html
+import Html exposing (Html)
 import Html.Attributes
 
 
@@ -12,8 +12,31 @@ main =
     Html.main_ []
         [ Html.canvas
             [ Html.Attributes.style
-                [ ( "backgroundImage", "url(" + diagram + ")" )
+                [ ( "backgroundImage"
+                  , "url(" ++ diagram ++ ")"
+                  )
                 ]
             ]
             []
+        , drawLabels [ { text = "Main", x = 100, y = 200 } ]
         ]
+
+
+type alias Label =
+    { text : String, x : Int, y : Int }
+
+
+drawLabels : List Label -> Html Never
+drawLabels labels =
+    let
+        oneLabel { text, x, y } =
+            Html.label
+                [ Html.Attributes.style
+                    [ ( "position", "absolute" )
+                    , ( "top", (toString y) ++ "px" )
+                    , ( "left", (toString x) ++ "px" )
+                    ]
+                ]
+                [ Html.text text ]
+    in
+        Html.div [] (List.map oneLabel labels)
